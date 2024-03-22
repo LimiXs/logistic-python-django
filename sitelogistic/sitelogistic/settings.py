1839b1a615a526b9517eec29f14c27ed2fece826
Siarhei Khomich <limixserega@gmail.com>
2024-03-22 17:18:22 +0300

    fixing some bugs with css

--- sitelogistic/sitelogistic/settings.py
+++ sitelogistic/sitelogistic/settings.py
@@ -17,6 +17,20 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+# STATIC_URL = '/static/'
+#
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'trade_logistic/static'),]
+#
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -127,13 +141,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+
 
 # STATICFILES_DIRS = ('static',)
 
